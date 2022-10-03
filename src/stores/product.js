4b27ee53e362407075b2c1309fa0799e67a4df82
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-10-03 17:47:13 +0200

    Convert Product Store to Setup store 🔃✨

--- src/stores/product.js
+++ src/stores/product.js
@@ -1,17 +1,18 @@
+import { ref, computed } from 'vue';
 import { defineStore, acceptHMRUpdate } from 'pinia';
 
-export const useProductStore = defineStore('product', {
-  state: () => ({
-    products: []
-  }),
-  getters: {
-    product: (state) => (id) => state.products.find((prod) => prod.id === id)
-  },
-  actions: {
-    async fill() {
-      this.products = (await import('@/data/products.json')).default;
-    }
+export const useProductStore = defineStore('product', () => {
+  const products = ref([]);
+
+  const product = computed(
+    () => (id) => products.value.find((prod) => prod.id === id)
+  );
+
+  async function fill() {
+    products.value = (await import('@/data/products.json')).default;
   }
+
+  return { products, product, fill };
 });
 
 if (import.meta.hot) {
