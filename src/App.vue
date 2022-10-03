1b3db42421f10befc31afa7057d1964a41bc3de1
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-10-04 00:00:00 +0200

    Subscribe to state - implement undo for cart store

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <script setup>
+import { ref, reactive } from 'vue';
 import { storeToRefs } from 'pinia';
 import TheHeader from '@/components/TheHeader.vue';
 import ProductCard from '@/components/ProductCard.vue';
 import { useProductStore } from '@/stores/product.js';
 import { useCartStore } from '@/stores/cart.js';
 
+const cartStore = useCartStore();
+
 const { products } = storeToRefs(useProductStore());
 const { fill } = useProductStore();
-const { addItems } = useCartStore();
+const { addItems } = cartStore;
 
-useCartStore().$onAction(({ name, store, args, after, onError }) => {
+const history = reactive([]);
+const doingHistory = ref(false);
+history.push(JSON.stringify(cartStore.$state));
+
+// Undo last action
+const undo = () => {
+  if (history.length === 1) return;
+  doingHistory.value = true;
+  history.pop();
+  cartStore.$state = JSON.parse(history.at(-1));
+  doingHistory.value = false;
+};
+
+// State Subscription
+cartStore.$subscribe((mutation, state) => {
+  if (!doingHistory.value) {
+    history.push(JSON.stringify(state));
+  }
+});
+
+// Action Subscription
+cartStore.$onAction(({ name, store, args, after, onError }) => {
   console.log(`Action ${name} was called on store ${store.$id}`, args);
 
   if (name === 'checkout') {
@@ -30,6 +54,9 @@ fill();
 <template>
   <div class="container">
     <TheHeader />
+    <div class="mb-5 flex justify-end">
+      <AppButton @click="undo">Undo</AppButton>
+    </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
         v-for="product in products"
